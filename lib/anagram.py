9f38af844ac3593641fd0cf4bcdbf91dbd3bcc30
Christine Mumbi <christinemumbi286@gmail.com>
2025-11-25 02:44:36 +0300

    Adds the Anagram class with its methods

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, word_list):
+        if not isinstance(word_list, list):
+            raise ValueError("Input must be a list of strings")
+        return [word for word in word_list if self.is_anagram(word)]
+
+    def is_anagram(self, other_word):
+        if not isinstance(other_word, str):
+            raise ValueError("Input must be a string")
+        return sorted(self.word) == sorted(other_word.lower())
+
+    def __str__(self):
+        return f"Anagram of: {self.word}"   
+    pass
